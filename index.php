4b41f280d3b506b0846b083a881483e9b0545f93
mamri <alazdi2007@gmail.com>
2018-02-22 14:59:24 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html>
+<head>
+
+
+<meta charset="utf-8" />
+
+<title>
+  Bootstrap Themes
+</title>
+
+
+<!-- styles -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link href="css/style.scss.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/styls.css" rel="stylesheet" type="text/css">
+	
+</head>
+
+<body >
+  <div class="">
+
+  
+  
+  <div class="container">
+
+<div class="hidden-xs">
+
+  <div class="row row-padded row-bordered row-centered">
+    <div class="col-sm-12">
+
+      <div class="row">
+        <div class="col-md-4 m-b-lg">
+          <div class="panel panel-default panel-profile m-b-0">
+            <div class="panel-heading" style="background-image: url(//cdn.shopify.com/s/files/1/0691/5403/t/143/assets/insta-2.jpg?3610006198801933811);"></div>
+            <div class="panel-body text-center">
+              <img class="panel-profile-img" src="//cdn.shopify.com/s/files/1/0691/5403/t/143/assets/avatar-fat.jpg?3610006198801933811">
+              <h5 class="panel-title">Jacob Thornton</h5>
+              <p class="m-b">Creator of Bootstrap, engineer at Twitter &amp; Medium. Founder of Bumpers.</p>
+              <a href="//github.com/fat" target="_blank" class="btn btn-primary-outline btn-sm m-b">
+                <span class="icon icon-add-user"></span> View GitHub
+              </a>
+            </div>
+          </div>
+        </div>
+
+<div class="" style="background-color: #eee; width: 700px">Width 70%</div>
+		
+      </div>
+    </div>
+  </div>
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+  <!-- Fixed panel for notifications -->
+  <!-- Disabled right now -->
+  
+
+</div>
+
+  </div>
+
+
+
+  </div>
+
+  <!-- General JavaScript -->
+  <!-- jquery.js -->
+
+
+<!-- bootstrap.js -->
+
+
+
+
+
+
+
+
+
+</body>
+</html>
